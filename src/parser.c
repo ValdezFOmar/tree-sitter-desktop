0badae29dc9a6c8f46e2088c8b435688ebd6f40e
Omar Valdez <omarantoniovaldezf2@gmail.com>
2026-02-23 20:09:40 -0800

    chore: release v1.1.1

--- src/parser.c
+++ src/parser.c
@@ -1486,7 +1486,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_desktop(void) {
     .metadata = {
       .major_version = 1,
       .minor_version = 1,
-      .patch_version = 0,
+      .patch_version = 1,
     },
   };
   return &language;
